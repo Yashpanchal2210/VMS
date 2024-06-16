abf899bd40827ea79828ade19158cdbf6b53e662
Yash Panchal <88079566+Yashpanchal2210@users.noreply.github.com>
2024-06-16 13:48:29 +0530

    merge

--- RationScale.aspx.cs
+++ RationScale.aspx.cs
@@ -17,12 +17,15 @@ namespace VMS_1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!HttpContext.Current.User.Identity.IsAuthenticated)
+            if (!IsPostBack)
             {
-                FormsAuthentication.RedirectToLoginPage();
+                if (!HttpContext.Current.User.Identity.IsAuthenticated)
+                {
+                    FormsAuthentication.RedirectToLoginPage();
+                }
+                GetItemNames();
+                BindGridView();
             }
-            GetItemNames();
-            BindGridView();
         }
 
         protected void SubmitButton_Click(object sender, EventArgs e)
@@ -42,7 +45,6 @@ namespace VMS_1
 
                     for (int i = 0; i < itemname.Length; i++)
                     {
-
                         SqlCommand checkCmd = new SqlCommand("SELECT COUNT(*) FROM RationScale WHERE ItemName = @ItemName", conn);
                         checkCmd.Parameters.AddWithValue("@ItemName", itemname[i]);
 
@@ -84,7 +86,6 @@ namespace VMS_1
             }
         }
 
-
         private int GetItemIdByName(string itemName)
         {
             int itemId = 0;
@@ -109,12 +110,11 @@ namespace VMS_1
             return itemId;
         }
 
-
         [WebMethod]
         public static List<object> GetItemNames()
         {
             string connStr = ConfigurationManager.ConnectionStrings["InsProjConnectionString"].ConnectionString;
-            string query = "SELECT * FROM AlternateItem";
+            string query = "SELECT iLueItem, Id FROM BasicLieuItems";
             DataTable dt = new DataTable();
 
             using (SqlConnection conn = new SqlConnection(connStr))
@@ -131,8 +131,8 @@ namespace VMS_1
             {
                 items.Add(new
                 {
-                    Text = row["AltItemName"].ToString(),
-                    Value = row["AltItemID"].ToString(),
+                    Text = row["iLueItem"].ToString(),
+                    Value = row["Id"].ToString(),
                 });
             }
 
@@ -153,8 +153,6 @@ namespace VMS_1
                     DataTable dt = new DataTable();
                     da.Fill(dt);
 
-                    //GridView1.DataSource = dt;
-                    //GridView1.DataBind();
                     GridViewRationScale.DataSource = dt;
                     GridViewRationScale.DataBind();
                 }
@@ -165,5 +163,92 @@ namespace VMS_1
             }
         }
 
+        protected void GridViewRationScale_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridViewRationScale.EditIndex = e.NewEditIndex;
+            BindGridView();
+
+            try
+            {
+                DropDownList ddlItemName = (DropDownList)GridViewRationScale.Rows[e.NewEditIndex].FindControl("ddlItemName");
+                if (ddlItemName != null)
+                {
+                    List<object> items = GetItemNames();
+                    ddlItemName.DataSource = items;
+                    ddlItemName.DataTextField = "Text";
+                    ddlItemName.DataValueField = "Value";
+                    ddlItemName.DataBind();
+
+                    GridViewRow row = GridViewRationScale.Rows[e.NewEditIndex];
+                    if (row != null && row.DataItem != null)
+                    {
+                        string currentItemName = ((DataRowView)row.DataItem)["ItemName"].ToString();
+                        if (ddlItemName.Items.FindByText(currentItemName) != null)
+                        {
+                            ddlItemName.SelectedValue = currentItemName;
+                        }
+                    }
+                }
+                else
+                {
+                    lblStatus.Text = "DropDownList ddlItemName not found.";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = "An error occurred while editing: " + ex.Message;
+            }
+        }
+
+        protected void GridViewRationScale_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["InsProjConnectionString"].ConnectionString;
+
+            int id = (int)e.Keys["ID"];
+            DropDownList ddlItemName = (DropDownList)GridViewRationScale.Rows[e.RowIndex].FindControl("ddlItemName");
+            string itemname = ddlItemName.SelectedItem.Text;
+            string rate = (string)e.NewValues["Rate"];
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                string query = "UPDATE RationScale SET ItemName=@ItemName, Rate=@Rate WHERE Id=@ID";
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@ItemName", itemname);
+                cmd.Parameters.AddWithValue("@Rate", rate);
+                cmd.Parameters.AddWithValue("@ID", id);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            GridViewRationScale.EditIndex = -1;
+            BindGridView();
+        }
+
+        protected void GridViewRationScale_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridViewRationScale.EditIndex = -1;
+            BindGridView();
+        }
+
+        protected void GridViewRationScale_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["InsProjConnectionString"].ConnectionString;
+            int id = (int)e.Keys["ID"];
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                string query = "DELETE FROM RationScale WHERE Id=@ID";
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@ID", id);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            BindGridView();
+        }
     }
 }
